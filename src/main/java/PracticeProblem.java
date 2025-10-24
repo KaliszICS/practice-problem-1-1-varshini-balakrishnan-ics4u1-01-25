a123ef567e8b16d726c3eb83bd10d3317157b6b7
varshini-balakrishnan-ics4u1-01-25 <varshini.balakrishnan@student.tdsb.on.ca>
2025-10-24 01:00:36 +0000

    PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,20 +1,67 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static void main(String[] args) {
+		q1();
+		q2();
+		q3();
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		int[][] nums = {
+			{1,2,3},
+			{4,5,6},
+			{7,8,9}
+		};
+	System.out.println("Sum of all elements: " + sum2D(nums));
+	}
+	public static int sum2D(int[][] arr) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				sum += arr[i][j];
+		}
+	}
+		return sum;
 	}
 
+
 	public static void q2() {
-		//Write question 2 code here
+		int[][] nums = {
+			{1,2,3},
+			{4,5,6},
+			{7,8,9}
+		};
+		int row = 1; 
+		System.out.println("Sum of row " + row + ": " + sumRow(nums, row));
+	}
+	public static int sumRow(int[][] arr, int row) {
+			int sum = 0;
+			if (row >= 0 && row < arr.length) {
+				for (int j = 0; j < arr[row].length; j++) {
+					sum += arr[row][j];
+				}
+			}
+		return sum;
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		int[][] nums = {
+			{1,2,3},
+			{4,5,6},
+			{7,8,9}
+		};
+		int column = 2;
+		System.out.println("Sum of column " + column + ": " + sumColumn(nums, column));
 	}
+		public static int sumColumn(int [][] arr, int column) {
+			int sum = 0;
+			for (int i = 0; i < arr.length; i++) {
+				if (column >= 0 && column < arr[i].length) {
+					sum += arr[i][column];
+				}
+			}
+			return sum;
+		}
 
 	public static void q4() {
 		//Write question 4 code here
